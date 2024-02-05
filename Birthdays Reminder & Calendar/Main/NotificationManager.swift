cb32fa379ff0833771c1933c7679744fbc04b64c
Daniil <idanik.mega@gmail.com>
2024-02-05 13:20:38 +0300

    bug fix

---
+++ Birthdays Reminder & Calendar/Main/NotificationManager.swift
@@ -0,0 +1,43 @@
+import UserNotifications
+
+class NotificationManager {
+    static let shared = NotificationManager()
+    
+    private init() {}        
+    
+    func scheduleBirthdayNotification(for birthday: Birthdays) {
+        let content = UNMutableNotificationContent()
+        content.title = NSLocalizedString("mainPage.notification.title", comment: "")
+        content.body = "\(birthday.nameBirthdays ?? "") \(birthday.surnameBirthdays ?? "")"
+        content.sound = UNNotificationSound.default
+        
+        guard let birthdayDate = birthday.dateBirthdays,
+              let triggerDate = dateFormatter.date(from: birthdayDate) else {
+            return
+        }
+        
+        var triggerDateComponents = calendar.dateComponents([.month, .day, .hour, .minute], from: triggerDate)
+        triggerDateComponents.hour = 13
+        triggerDateComponents.minute = 17
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDateComponents, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("Error scheduling notification: \(error.localizedDescription)")
+            } else {
+                print("Notification scheduled successfully")
+            }
+        }
+    }
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd.MM.yyyy"
+        return formatter
+    }()
+    
+    private let calendar = Calendar.current
+}
